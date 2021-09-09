a1a071a305735ae4659fe8a592d44e3a7a3b6a2a
isaacBejarano <isaacbejaranopro@gmail.com>
2021-09-09 14:00:11 +0200

    hotfix: remove local ntl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { HomeComponent } from './pages/home/home.component';
 
 // components
-// import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // NavigationComponent
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
