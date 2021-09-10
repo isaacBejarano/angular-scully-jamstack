4018caac3f5aab301a7f4a174ad2cf9c7e9c651a
isaacBejarano <isaacbejaranopro@gmail.com>
2021-09-10 19:52:28 +0200

    scully plugins test 7

--- scully.angular-mission1.config.ts
+++ scully.angular-mission1.config.ts
@@ -1,11 +1,21 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+
 import { MinifyHtml } from 'scully-plugin-minify-html'; // <- dep: [html-minifier, tslib]
+import { MinifyHtmlOptions } from 'scully-plugin-minify-html/lib';
+
+
 // const { RemoveUnusedCSSPlugin } = require('scully-plugin-remove-unused-css'); // <- dep: [extract-css, purgeCSS]
 
-// import '@notiz/scully-plugin-lazy-images';
 
 // FIXME: "scully-plugin-minify-html" is using deprecated plugin type:"render"  use "postProcessByHtml" instead
 const postRenderers = [MinifyHtml];
+
+const minifyHtmlOptions: MinifyHtmlOptions = {
+  minifyOptions: {
+    removeComments: false,
+  },
+};
+setPluginConfig(MinifyHtml, 'render', minifyHtmlOptions);
 
 export const config: ScullyConfig = {
   projectRoot: './src',
