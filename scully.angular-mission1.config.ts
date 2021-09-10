f3543b495dcd2fe2ebc0c1934dee20b3d8e4cb4d
isaacBejarano <isaacbejaranopro@gmail.com>
2021-09-10 20:16:37 +0200

    scully-plugin-minify-html well configured

--- scully.angular-mission1.config.ts
+++ scully.angular-mission1.config.ts
@@ -12,10 +12,34 @@ const postRenderers = [MinifyHtml];
 
 const minifyHtmlOptions: MinifyHtmlOptions = {
   minifyOptions: {
-    removeComments: true,
+    // caseSensitive: true,
+    // removeComments: true,
+    // collapseWhitespace: true,
+    // collapseBooleanAttributes: true,
+    // removeRedundantAttributes: true,
+    // useShortDoctype: true,
+    // removeEmptyAttributes: true,
+    // minifyCSS: true,
+    // minifyJS: true,
+    // removeScriptTypeAttributes: true,
+    // removeStyleLinkTypeAttributes: true,
+    // // don't remove attribute quotes, not all social media platforms can parse this over-optimization
+    // removeAttributeQuotes: false,
+    // // don't remove optional tags, like the head, not all social media platforms can parse this over-optimization
+    // removeOptionalTags: false,
+    // // scully specific HTML comments
+    // // this will always be added in the final minifyOptions config
+    // ignoreCustomComments: [
+    //   /scullyContent-(begin|end)/
+    // ],
+    // // scully specific data injection
+    // // this will always be added in the final minifyOptions config
+    // ignoreCustomFragments: [
+    //   /\/\*\* ___SCULLY_STATE_(START|END)___ \*\//
+    // ]
   },
 };
-setPluginConfig(MinifyHtml, 'render', minifyHtmlOptions);
+setPluginConfig(MinifyHtml, 'postProcessByHtml', minifyHtmlOptions);
 
 export const config: ScullyConfig = {
   projectRoot: './src',
