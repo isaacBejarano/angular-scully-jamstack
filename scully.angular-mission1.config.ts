c68ae62506b374d7de714417c0209372c106a5f8
isaacBejarano <isaacbejaranopro@gmail.com>
2021-09-10 19:14:02 +0200

    scully plugind 7

--- scully.angular-mission1.config.ts
+++ scully.angular-mission1.config.ts
@@ -1,22 +1,17 @@
 import { ScullyConfig } from '@scullyio/scully';
-// const { MinifyHtml } = require('scully-plugin-minify-html'); // <- dep: [html-minifier, tslib]
+const { MinifyHtml } = require('scully-plugin-minify-html'); // <- dep: [html-minifier, tslib]
 // const { RemoveUnusedCSSPlugin } = require('scully-plugin-remove-unused-css'); // <- dep: [extract-css, purgeCSS]
-// require('@notiz/scully-plugin-lazy-images'); // 'lazyImages'
 
 // import '@notiz/scully-plugin-lazy-images';
 
 // FIXME: "scully-plugin-minify-html" is using deprecated plugin type:"render"  use "postProcessByHtml" instead
-// const postRenderers = [
-  // MinifyHtml,
-  // RemoveUnusedCSSPlugin,
-  // 'lazyImages',
-// ];
+const postRenderers = [MinifyHtml];
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'angular-mission1',
   outDir: './dist/static',
-  // defaultPostRenderers: [...postRenderers],
+  defaultPostRenderers: [...postRenderers],
   routes: {
     /* Plugins fos specific Routes */
     // '/blog/:slug': {
@@ -24,7 +19,7 @@ export const config: ScullyConfig = {
     //   slug: {
     //     folder: './content/blog'
     //   },
-    //   postRenderers: ['lazyImages'] // <== HERE!
+    //   postRenderers: ['MinifyHtml'] // <== HERE!
     // }
   },
 };
