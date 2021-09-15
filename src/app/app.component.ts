7842b8dffbd036c1d67a36c995a6d6e0848bfd16
isaacBejarano <isaacbejaranopro@gmail.com>
2021-09-15 18:07:59 +0200

    vanilla algolia added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
+// import algoliasearch from 'algoliasearch/lite';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,12 @@ import { environment } from 'src/environments/environment';
 export class AppComponent {
   title = 'angular-mission1';
   mode = environment.mode;
+  /* ALGOLIA */
+  // algoliaConfig = {
+  //   indexName: 'angular-missions-search',
+  //   searchClient: algoliasearch(
+  //     'S6R1UCM89C',
+  //     '9faf263189cf1ab601a62bbd0bc48039'
+  //   ),
+  // };
 }
-
-// FIXME: prueba ANGULA PLUGIN: https://www.algolia.com/blog/product/bringing-search-to-angular/
-// https://community.algolia.com/angular-instantsearch/
