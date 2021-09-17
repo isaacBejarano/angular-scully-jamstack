0aa521b078e27d91a56d4cc8ddbbd1f6b02b53a3
Isaac Bejarano <44377797+isaacBejarano@users.noreply.github.com>
2021-09-17 18:20:49 +0200

    Dev (#11)
    * merged master
    
    * vanilla algolia added
    
    * trying Ubuntu Focal

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -68,3 +68,10 @@ import 'zone.js'; // Included with Angular CLI.
  */
 // tslint:disable-next-line: align
 import 'zone.js/dist/task-tracking';
+
+/***************************************************************************************************
+ * ALGOLIA
+ */
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
